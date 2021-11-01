5f3b9e318c0437102ee22e259e93ef1f4bc05e28
parkGunWoong <bung1438@gmail.com>
2021-11-01 23:56:55 +0900

    다이나믹 라우팅 적용

--- pages/view/[id].tsx
+++ pages/view/[id].tsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { NextRouter, useRouter } from 'next/router';
+import axios from 'axios';
 
 export default function Detail() {
-  return <>Detail</>;
+  const router: NextRouter = useRouter();
+  const id: string | string[] | undefined = router.query.id;
+
+  const API_URL = `http://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  const fetchData = () => {
+    axios.get(API_URL).then((res) => {
+      console.log(res.data);
+    });
+  };
+
+  useEffect(() => {
+    // id를 받아올 때 데이터 fetching
+    if (id) fetchData();
+  }, [id]);
+
+  return <p>id : {id}</p>;
 }
--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import { Container, Item, ItemImage, ItemName, ItemPrice } from '../styles/ItemList';
 
 interface PropType {
@@ -8,17 +9,19 @@ interface PropType {
 export default function ItemList(props: PropType) {
   const { itemList } = props;
 
-  console.log(itemList);
-
   return (
     <Container>
       {itemList.map((item) => {
         return (
-          <Item key={item.id}>
-            <ItemImage src={item.image_link} alt={item.name} />
-            <ItemName>{item.name}</ItemName>
-            <ItemPrice>${item.price}</ItemPrice>
-          </Item>
+          <Link key={item.id} href={`/view/${item.id}`}>
+            <a>
+              <Item>
+                <ItemImage src={item.image_link} alt={item.name} />
+                <ItemName>{item.name}</ItemName>
+                <ItemPrice>${item.price}</ItemPrice>
+              </Item>
+            </a>
+          </Link>
         );
       })}
     </Container>
