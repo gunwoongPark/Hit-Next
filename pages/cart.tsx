7761addba71064b3b070793dc7e5ed1698caa886
parkGunWoong <bung1438@gmail.com>
2021-11-21 20:21:34 +0900

    Cart 페이지 메타 데이터 추가

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Container } from '../styles/components/Cart';
+import Head from 'next/head';
 
 export default function Cart() {
-  return <Container>Cart!!!</Container>;
+  return (
+    <>
+      <Head>
+        <title>CART</title>
+        <meta name="description" content="This is Cart page" />
+      </Head>
+      <Container>Cart!!!</Container>
+    </>
+  );
 }
