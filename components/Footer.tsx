e1a40c09e3541f8cebd69507d9ff06a12f86c9a5
parkGunWoong <bung1438@gmail.com>
2021-11-21 16:39:55 +0900

    프로젝트 재생성

--- components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Container } from './styles/Footer';
-import { NextRouter, useRouter } from 'next/router';
-
-export default function Footer() {
-  const router: NextRouter = useRouter();
-  return <Container asPath={router.asPath}>© 2021 Copyright gunwoongPark</Container>;
-}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,8 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
