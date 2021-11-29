d4ea81cc8e88deca4bec0de01375c1867622966f
parkGunWoong <bung1438@gmail.com>
2021-11-29 23:14:14 +0900

    의미없는 admin 경로감추기

--- components/Header.tsx
+++ components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
       <NavContainer>
         <Nav onClick={() => goLink('home')}>Home</Nav>
         <Nav onClick={() => goLink('cart')}>Cart</Nav>
-        <Nav onClick={() => goLink('admin')}>Admin</Nav>
+        {/* <Nav onClick={() => goLink('admin')}>Admin</Nav> */}
         <Nav onClick={() => goLink('about')}>About</Nav>
       </NavContainer>
     </Container>
